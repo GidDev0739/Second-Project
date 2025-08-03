900b23b8f211b5a604ae0c3deda8fb4114b8423f
Olaniyi Gideon <olaniyigideon91@gmail.com>
2025-08-03 17:03:42 +0100

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*let firstName = "Olaniyi";
+let lastName = "Akanbi";
+let greeting = "Hello"
+
+if (firstName === "Olaniyi", lastName === "Akanbi");
+
+alert(greeting, firstName, lastName);
+
+console.log(greeting, firstName, lastName);
+
+const header = document.getElementById("header");
+const myheader = document.getElementsByClassName("myheader");
+const con1 = document.getElementsByClassName("con1");
+const con1acon1bcon1c = document.getElementsByClassName("con1a", "con1b", "con1c");
+*/
